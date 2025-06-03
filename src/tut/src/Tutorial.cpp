420f1fd2027bc7bf422b1e3c2ebb1553dffe4c1e
Hans Buss <hans.buss@mailfence.com>
2025-06-03 15:48:54 -0300

    Minimal working example of gurobi running on openRoad

--- src/tut/src/Tutorial.cpp
+++ src/tut/src/Tutorial.cpp
@@ -2,8 +2,8 @@
 #include "odb/db.h"
 #include "ord/OpenRoad.hh"
 #include "utl/Logger.h"
-
 #include <iostream>
+#include "gurobi_c++.h"
 
 /*
  * How to use:
@@ -77,8 +77,46 @@ Tutorial::printPins()
 void
 Tutorial::printHPWLs()
 {
-  //TODO
-  //Challenge: Traverse all nets printing the total HPWL
+	try {
+	// Create an environment
+	GRBEnv env = GRBEnv(true);
+     logger_->report("env");
+	env.set("LogFile", "mip1.log");
+	env.start();
+
+
+    //Create an empty model
+    GRBModel model = GRBModel(env);
+
+    // Create variables
+    GRBVar x = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "x");
+    GRBVar y = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "y");
+    GRBVar z = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "z");
+
+    // Set objective: maximize x + y + 2 z
+    model.setObjective(x + y + 2 * z, GRB_MAXIMIZE);
+
+    // Add constraint: x + 2 y + 3 z <= 4
+    model.addConstr(x + 2 * y + 3 * z <= 4, "c0");
+
+    // Add constraint: x + y >= 1
+    model.addConstr(x + y >= 1, "c1");
+
+    // Optimize model
+    model.optimize();
+
+    std::cout << x.get(GRB_StringAttr_VarName) << " "
+         << x.get(GRB_DoubleAttr_X) << std::endl;
+    std::cout << y.get(GRB_StringAttr_VarName) << " "
+         << y.get(GRB_DoubleAttr_X) << std::endl;
+    std::cout << z.get(GRB_StringAttr_VarName) << " "
+         << z.get(GRB_DoubleAttr_X) << std::endl;
+	}  catch(GRBException e) {
+		std::cout << "Error code = " << e.getErrorCode() << std::endl;
+		std::cout << e.getMessage() << std::endl;
+	}
+
+	
 }
 
 Tutorial::~Tutorial()
